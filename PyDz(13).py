52c351f3af46911384985fb974986ff983501682
SCXBelka <scvifixbelka@gmail.com>
2023-03-11 00:38:34 +0900

    zadachi po rekyrsii

---
+++ PyDz(13).py
@@ -0,0 +1,9 @@
+num_a = int(input("Vvedite A: "))
+num_b = int(input("Vvedite B: "))
+def summ_rekyrs(na, nb):
+    if nb == 0: 
+        return 1
+    else:
+        return na * summ_rekyrs(na, nb - 1)        
+print(summ_rekyrs(num_a, num_b))
+
